1ca16b934b94773e4d57b44d2764fafa59687eca
Peter Bom Jakobsen <pejak1990@gmail.com>
2017-11-17 14:00:31 +0100

    simplifying main.

--- main.go
+++ main.go
@@ -56,12 +56,13 @@ var (
 	}
 )
 
-func readBinary(name string) ([]uint32, error) {
+// readBinary reads binary file in little endian format and returns the content
+// in slice of instructions. If there is an error, it will be of type ErrUnexpectedEOF.
+func readBinary(name string) (instructions []uint32, err error) {
 	b, err := ioutil.ReadFile(name)
 	if err != nil {
 		return nil, fmt.Errorf("could not open file: %v", err)
 	}
-	prog := []uint32{}
 	var data uint32
 	buf := bytes.NewReader(b)
 	for {
@@ -72,9 +73,9 @@ func readBinary(name string) ([]uint32, error) {
 			}
 			break
 		}
-		prog = append(prog, data)
+		instructions = append(instructions, data)
 	}
-	return prog, nil
+	return instructions, nil
 }
 
 const header = "PC\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\tx%v\t\n"
@@ -88,6 +89,7 @@ func conv(a []uint32) []interface{} {
 	return s
 }
 
+// gen generates a slice from 0 to 31.
 func gen() []interface{} {
 	v := make([]interface{}, 32)
 	for i := 0; i < 32; i++ {
@@ -97,6 +99,69 @@ func gen() []interface{} {
 }
 
 const usage = `Specify a binary file ending with '.bin'.`
+
+// execute decode and executes the instruction and store the results into the
+// registers.
+func execute(instr uint32, reg []uint32) {
+	opcode := instr & 0x7f
+	switch opcode {
+	case 0x13:
+		rd := (instr >> 7) & 0x1f
+		funct3 := (instr >> 12) & 0x7
+		rs1 := (instr >> 15) & 0x1f
+		switch funct3 {
+		case 0: // Addi
+			imm := (instr >> 20)
+			if imm>>11 == 1 {
+				// subtract
+				reg[rd] = reg[rs1] - (imm ^ 4095 + 1)
+			} else {
+				reg[rd] = reg[rs1] + imm
+			}
+		case 1: // Left shifting
+			shamt := (instr >> 20) & 0x1f
+			rest := (instr >> 25)
+			if rest != 0 {
+				fmt.Println("The encoding for left shifting is wrong:", rest)
+				os.Exit(1)
+			}
+			v := reg[rd]
+			v = v << shamt
+			reg[rd] = v
+		case 5:
+			shamt := (instr >> 20) & 0x1f
+			rest := (instr >> 25)
+			if rest != 0 && rest != 32 {
+				fmt.Println("The encoding for right shifting is wrong:", rest)
+				os.Exit(1)
+			}
+
+			v := reg[rd]
+			if rest == 0 {
+				v = v >> shamt
+				reg[rd] = v
+			} else {
+				vv := int32(v)
+				vv = vv >> shamt
+				reg[rd] = uint32(vv)
+			}
+		}
+	case 0x33:
+		rd := (instr >> 7) & 0x1f
+		rs1 := (instr >> 15) & 0x1f
+		rs2 := (instr >> 20) & 0x1f
+		reg[rd] = reg[rs1] + reg[rs2]
+	case 0x37:
+		rd := (instr >> 7) & 0x1f
+		imm := (instr >> 12) << 12
+		reg[rd] = imm
+	case 0x73:
+		// Ignore Ecall for now
+	default:
+		fmt.Printf("Opcode %d not yet implemented\n", opcode)
+	}
+
+}
 
 func main() {
 	args := os.Args
@@ -121,73 +186,15 @@ func main() {
 	w.Init(os.Stdout, 0, 0, 2, ' ', tabwriter.AlignRight)
 	fmt.Fprintf(w, header, gen()...)
 
-	pc := 0
+	pc := uint(0)
 	for {
 		instr := prog[pc]
-		opcode := instr & 0x7f
-
-		switch opcode {
-		case 0x13:
-			rd := (instr >> 7) & 0x1f
-			funct3 := (instr >> 12) & 0x7
-			rs1 := (instr >> 15) & 0x1f
-			switch funct3 {
-			case 0: // Addi
-				imm := (instr >> 20)
-				if imm>>11 == 1 {
-					// subtract
-					reg[rd] = reg[rs1] - (imm ^ 4095 + 1)
-				} else {
-					reg[rd] = reg[rs1] + imm
-				}
-			case 1: // Left shifting
-				shamt := (instr >> 20) & 0x1f
-				rest := (instr >> 25)
-				if rest != 0 {
-					fmt.Println("The encoding for left shifting is wrong:", rest)
-					os.Exit(1)
-				}
-				v := reg[rd]
-				v = v << shamt
-				reg[rd] = v
-			case 5:
-				shamt := (instr >> 20) & 0x1f
-				rest := (instr >> 25)
-				if rest != 0 && rest != 32 {
-					fmt.Println("The encoding for right shifting is wrong:", rest)
-					os.Exit(1)
-				}
-
-				v := reg[rd]
-				if rest == 0 {
-					v = v >> shamt
-					reg[rd] = v
-				} else {
-					vv := int32(v)
-					vv = vv >> shamt
-					reg[rd] = uint32(vv)
-				}
-			}
-		case 0x33:
-			rd := (instr >> 7) & 0x1f
-			rs1 := (instr >> 15) & 0x1f
-			rs2 := (instr >> 20) & 0x1f
-			reg[rd] = reg[rs1] + reg[rs2]
-		case 0x37:
-			rd := (instr >> 7) & 0x1f
-			imm := (instr >> 12) << 12
-			reg[rd] = imm
-		case 0x73:
-			// Ignore Ecall for now
-		default:
-			fmt.Printf("Opcode %d not yet implemented\n", opcode)
-		}
-
+		execute(instr, reg)
 		fmt.Fprintf(w, "%v\t", pc)
 		fmt.Fprintf(w, body, conv(reg)...)
 
 		pc++
-		if pc >= len(prog) {
+		if pc >= uint(len(prog)) {
 			break
 		}
 	}
